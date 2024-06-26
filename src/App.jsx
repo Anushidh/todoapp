40efa761987f25634fe2b0e3796ec5d15cdeea4d
Anushidh <anushidh101@gmail.com>
2024-06-26 21:04:42 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Todo from './components/Todo'
-
+import Header from './components/Header.jsx'
+import Todo from './components/Todo.jsx'
+import './App.css'
 function App() {
   return (
     <div className="App">
+      <Header />
       <Todo />
     </div>
   )
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
+
+import Form from "./Form"
+import TodoList from "./TodoList"
+import Footer from "./Footer"
 export default function Todo() {
+  const [todos, setTodos] = useState([])
+  const completedTodos = todos.filter((todo) => todo.done).length
+  const totalTodos = todos.length
   return (
     <div>
-      <form>
-        <input type="text" />
-        <button type="submit">Add</button>
-      </form>
+      <Form todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos}/>
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
     </div>
-    )
+  )
 }
